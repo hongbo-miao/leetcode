d72513f80d972245888ae8d1c10d5df0565d6aea
Hongbo Miao <hongbo-miao@Hongbos-MacBook-Pro.local>
2018-06-10 20:43:58 -0700

    feat: algorithms

---
+++ 212. Word Search II.js
@@ -0,0 +1,71 @@
+// Given a 2D board and a list of words from the dictionary, find all words in the board.
+//
+// Each word must be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
+//
+// Example:
+//
+// Input:
+//   words = ["oath","pea","eat","rain"] and board =
+//   [
+//     ['o','a','a','n'],
+//     ['e','t','a','e'],
+//     ['i','h','k','r'],
+//     ['i','f','l','v']
+//   ]
+//
+// Output: ["eat","oath"]
+//
+// Note:
+// You may assume that all inputs are consist of lowercase letters a-z.
+
+/**
+ * @param {character[][]} board
+ * @param {string[]} words
+ * @return {string[]}
+ */
+function findWords(board, words) {
+  const root = buildTrie(words);
+  let res = [];
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      searchWord(root, i, j, board, res);
+    }
+  }
+
+  return res;
+}
+
+function buildTrie(words) {
+  let root = {};
+
+  for (let word of words) {
+    let node = root;
+
+    word.split('').forEach(c => node = (node[c] ? node[c] : node[c] = {}));
+    node.word = word;
+  }
+
+  return root;
+}
+
+function searchWord(node, i, j, board, res) {
+  if (node.word) {
+    res.push(node.word);
+    node.word = null;
+  }
+
+  if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) return;
+  if (board[i][j] === '#' || !node[board[i][j]]) return;
+
+  const c = board[i][j];
+
+  board[i][j] = '#'; // mark visited
+
+  searchWord(node[c], i + 1, j, board, res);
+  searchWord(node[c], i - 1, j, board, res);
+  searchWord(node[c], i, j + 1, board, res);
+  searchWord(node[c], i, j - 1, board, res);
+
+  board[i][j] = c;  // reset
+}
---
+++ 68. Text Justification.js
@@ -0,0 +1,102 @@
+// Given an array of words and a width maxWidth, format the text such that each line has exactly maxWidth characters and is fully (left and right) justified.
+//
+// You should pack your words in a greedy approach; that is, pack as many words as you can in each line. Pad extra spaces ' ' when necessary so that each line has exactly maxWidth characters.
+//
+// Extra spaces between words should be distributed as evenly as possible. If the number of spaces on a line do not divide evenly between words, the empty slots on the left will be assigned more spaces than the slots on the right.
+//
+// For the last line of text, it should be left justified and no extra space is inserted between words.
+//
+// Note:
+//
+//   A word is defined as a character sequence consisting of non-space characters only.
+//   Each word's length is guaranteed to be greater than 0 and not exceed maxWidth.
+//   The input array words contains at least one word.
+//
+// Example 1:
+//
+// Input:
+//   words = ["This", "is", "an", "example", "of", "text", "justification."]
+//   maxWidth = 16
+// Output:
+//   [
+//     "This    is    an",
+//     "example  of text",
+//     "justification.  "
+//   ]
+//
+// Example 2:
+//
+// Input:
+//   words = ["What","must","be","acknowledgment","shall","be"]
+//   maxWidth = 16
+// Output:
+//   [
+//     "What   must   be",
+//     "acknowledgment  ",
+//     "shall be        "
+//   ]
+// Explanation: Note that the last line is "shall be    " instead of "shall     be",
+//   because the last line must be left-justified instead of fully-justified.
+//   Note that the second line is also left-justified becase it contains only one word.
+//
+// Example 3:
+//
+// Input:
+//   words = ["Science","is","what","we","understand","well","enough","to","explain",
+//     "to","a","computer.","Art","is","everything","else","we","do"]
+// maxWidth = 20
+// Output:
+//   [
+//     "Science  is  what we",
+//     "understand      well",
+//     "enough to explain to",
+//     "a  computer.  Art is",
+//     "everything  else  we",
+//     "do                  "
+//   ]
+
+/**
+ * @param {string[]} words
+ * @param {number} maxWidth
+ * @return {string[]}
+ */
+function fullJustify(words, maxWidth) {
+  let res = [[]];
+  res[0].letters = 0;
+
+  for (let word of words) {
+    let row = res[res.length - 1];
+
+    if (row.length && row.letters + row.length + word.length > maxWidth) {
+      res.push([]);
+      row = res[res.length - 1];
+      row.letters = 0;
+    }
+
+    row.push(word);
+    row.letters += word.length;
+  }
+
+  for (let i = 0; i < res.length; i++) {
+    const row = res[i];
+
+    // only one word in the row or last row
+    if (row.length === 1 || i === res.length - 1) {
+      res[i] = row.join(' ') + ' '.repeat(maxWidth - row.letters - row.length + 1);
+    } else {
+      const spaces = maxWidth - row.letters;
+      const minSpaces = ' '.repeat(Math.floor(spaces / (row.length - 1)));
+      const addSpace = spaces % (row.length - 1);
+
+      let line = row[0];
+
+      for (let j = 1; j < row.length; j++) {
+        line += minSpaces + (j <= addSpace ? ' ' : '') + row[j];
+      }
+
+      res[i] = line;
+    }
+  }
+
+  return res;
+}
