7532f01ee8ca5c6fd809c34ee118d5f55407a3dc
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-03-09 23:22:27 -0800

    feat: Single Number

---
+++ Python/136. Single Number.py
@@ -0,0 +1,27 @@
+# Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+#
+# Note:
+#
+# Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+#
+# Example 1:
+#
+# Input: [2,2,1]
+# Output: 1
+#
+# Example 2:
+#
+# Input: [4,1,2,1,2]
+# Output: 4
+
+
+import operator
+from functools import reduce
+
+
+class Solution:
+    def singleNumber1(self, nums: List[int]) -> int:
+        return reduce(operator.xor, nums)
+
+    def singleNumber(self, nums: List[int]) -> int:
+        return reduce(lambda x, y: x ^ y, nums)
