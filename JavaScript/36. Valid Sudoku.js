14e79c507d2fca6fbb41844c2e4262b4f90b099f
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-09-23 18:43:59 -0700

    feat: add new solution for isValidSudoku

--- JavaScript/36. Valid Sudoku.js
+++ JavaScript/36. Valid Sudoku.js
@@ -51,6 +51,45 @@
  * @param {character[][]} board
  * @return {boolean}
  */
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+function isValidSudoku1(board) {
+  // row
+  for (let i = 0; i < 9; i++) {
+    let map = {};
+    for (let j = 0; j < 9; j++) {
+      if (map[board[i][j]]) return false;
+      if (board[i][j] !== '.') map[board[i][j]] = true;
+    }
+  }
+
+  // column
+  for (let j = 0; j < 9; j++) {
+    let map = {};
+    for (let i = 0; i < 9; i++) {
+      if (map[board[i][j]]) return false;
+      if (board[i][j] !== '.') map[board[i][j]] = true;
+    }
+  }
+
+  // box
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      let map = {};
+      for (let k = 3 * i; k < 3 * i + 3; k++) {
+        for (let l = 3 * j; l < 3 * j + 3; l++) {
+          if (map[board[k][l]]) return false;
+          if (board[k][l] !== '.') map[board[k][l]] = true;
+        }
+      }
+    }
+  }
+
+  return true;
+}
+
 function isValidSudoku(board) {
   // let row = Array(9).fill({});  // wrong, all will point to same {} reference
   // let row = [{}, {}, {}, {}, {}, {}, {}, {}, {}];  // correct
