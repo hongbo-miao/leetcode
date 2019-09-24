39d1e31b667b9193da56599630f8d77a5beeb402
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-09-23 20:02:00 -0700

    feat: add new solution for reverseString

--- JavaScript/344. Reverse String.js
+++ JavaScript/344. Reverse String.js
@@ -9,6 +9,13 @@
  */
 
 /** Cheating */
-function reverseString(s) {
+function reverseString1(s) {
   return s.split('').reverse().join('');
+}
+
+function reverseString(s) {
+  const l = s.length - 1;
+  for (let i = 0; i < Math.floor(s.length / 2); i++) {
+    [s[i], s[l - i]] = [s[l - i], s[i]];
+  }
 }
