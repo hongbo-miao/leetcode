4be3f9500bbb89832a77a9cf84b1341a71504eca
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-09-29 15:20:55 -0700

    feat: new solution for Number of Islands

--- JavaScript/200. Number of Islands.js
+++ JavaScript/200. Number of Islands.js
@@ -25,16 +25,27 @@
  * @return {number}
  */
 
-/** DFS */
+/** 1) DFS */
 // Similar
 // 547. Friend Circles
 function numIslands(grid) {
+  function go(i, j) {
+    if (grid[i][j] !== '1') return;
+
+    grid[i][j] = '*';  // go land piece as visited
+
+    if (i > 0) go(i - 1, j);  // up
+    if (i < grid.length - 1) go(i + 1, j);  // down
+    if (j > 0) go(i, j - 1);  // left
+    if (j < grid[i].length - 1) go(i, j + 1);  // right
+  }
+
   let count = 0;
 
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[i].length; j++) {
       if (grid[i][j] === '1') {
-        mark(grid, i, j);
+        go(i, j);
         count++;
       }
     }
@@ -43,13 +54,35 @@ function numIslands(grid) {
   return count;
 }
 
-function mark(grid, row, col) {
-  if (grid[row][col] === '1') {
-    grid[row][col] = '*'; // mark land piece as visited
 
-    if (row > 0) mark(grid, row - 1, col); // up
-    if (row < grid.length - 1) mark(grid, row + 1, col); // down
-    if (col > 0) mark(grid, row, col - 1); // left
-    if (col < grid[row].length - 1) mark(grid, row, col + 1);  // right
+/** 2) Similar to 1) */
+function numIslands(grid) {
+  if (grid.length === 0) return 0;
+
+  const h = grid.length;
+  const w = grid[0].length;
+
+  function go(i, j) {
+    if (i < 0 || i >= h || j < 0 || j >= w || grid[i][j] !== '1') {
+      return false;
+    }
+
+    grid[i][j] = '*';
+    go(i - 1, j);
+    go(i + 1, j);
+    go(i, j - 1);
+    go(i, j + 1);
+    return true;
   }
+
+  let count = 0;
+  for (let i = 0; i < h; i++) {
+    for (let j = 0; j < w; j++) {
+      if (go(i, j) === true) {
+        count++;
+      }
+    }
+  }
+
+  return count;
 }
