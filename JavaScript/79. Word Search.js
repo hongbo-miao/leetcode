b8e28269d7a9b304a87e91de1a64ddc751451011
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-09-30 15:25:24 -0700

    refact: Word Search

--- JavaScript/79. Word Search.js
+++ JavaScript/79. Word Search.js
@@ -26,27 +26,30 @@
 // time O(mn * 4^l), l = word.length
 // space O(mn + l)
 function exist(board, word) {
-  function find(i, j, k) {
-    if (i < 0 || j < 0 || i > board.length - 1 || j > board[0].length - 1) return false;
-    if (board[i][j] !== word[k]) return false;
+  if (board.length === 0) return false;
 
+  const h = board.length;
+  const w = board[0].length;
+
+  function go(i, j, k) {
+    if (i < 0 || j < 0 || i >= h || j >= w) return false;
+    if (board[i][j] !== word[k]) return false;
     if (k === word.length - 1) return true;
 
     board[i][j] = '*';      // mark as visited
 
-    if (find(i - 1, j, k + 1)) return true;  // up
-    if (find(i + 1, j, k + 1)) return true;  // down
-    if (find(i, j - 1, k + 1)) return true;  // left
-    if (find(i, j + 1, k + 1)) return true;  // right
+    if (go(i - 1, j, k + 1)) return true;  // up
+    if (go(i + 1, j, k + 1)) return true;  // down
+    if (go(i, j - 1, k + 1)) return true;  // left
+    if (go(i, j + 1, k + 1)) return true;  // right
 
     board[i][j] = word[k];  // reset
-
     return false;
   }
 
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      if (find(i, j, 0)) return true;
+  for (let i = 0; i < h; i++) {
+    for (let j = 0; j < w; j++) {
+      if (go(i, j, 0)) return true;
     }
   }
 
