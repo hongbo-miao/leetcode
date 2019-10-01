4c3735fd32c0f38cb5070291b559cac3c1cf9547
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-09-30 17:53:32 -0700

    feat: new solution for Top K Frequent Elements

--- JavaScript/347. Top K Frequent Elements.js
+++ JavaScript/347. Top K Frequent Elements.js
@@ -13,34 +13,52 @@
  * @return {number[]}
  */
 
-// Bucket sorting
+/** 1) Bucket sorting */
 // time O(n)
 // space O(n) array -> O(k) hashtable
-function topKFrequent(nums, k) {
-  const map = {};
-  let bucket = [];
-  let res = [];
-
+function topKFrequent1(nums, k) {
   // number frequency map
+  const map = {};
   for (let n of nums) {
-    if (map[n] === undefined) map[n] = 0;
+    if (map[n] == null) map[n] = 0;
     map[n]++;
   }
 
   // store map to a bucket based on frequency
+  const bucket = [];
   for (let n in map) {
     const freq = map[n];
-    if (bucket[freq] === undefined) bucket[freq] = [];
+    if (bucket[freq] == null) bucket[freq] = [];
     bucket[freq].push(Number(n));
   }
 
   // get most frequent numbers
+  const res = [];
   for (let freq = bucket.length - 1; freq >= 0; freq--) {
-    if (bucket[freq] === undefined) continue;
+    if (bucket[freq] == null) continue;
     res.push(...bucket[freq]);
 
     if (res.length === k) return res;
   }
 
   return res;
+}
+
+/** 2) */
+function topKFrequent(nums, k) {
+  const map = {};
+  for (let n of nums) {
+    if (map[n] == null) map[n] = 0;
+    map[n]++;
+  }
+
+  const arr = [];
+  Object.keys(map).forEach(n => {
+    arr.push({ n, count: map[n] });
+  });
+
+  return arr
+    .sort((a, b) => b.count - a.count)
+    .slice(0, k)
+    .map(a => Number(a.n));
 }
