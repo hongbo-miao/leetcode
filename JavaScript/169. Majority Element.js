182e5547d59f4fa8a4d9567cafaabfc87e76fec5
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-10-03 18:37:41 -0700

    refact: Majority Element

--- JavaScript/169. Majority Element.js
+++ JavaScript/169. Majority Element.js
@@ -19,7 +19,7 @@
 /** 1) Sorting */
 // time O(n log n)
 // space O(1)
-function majorityElement(nums) {
+function majorityElement1(nums) {
   nums.sort((a, b) => a - b);
   return nums[Math.floor(nums.length / 2)];
 }
@@ -27,11 +27,11 @@ function majorityElement(nums) {
 /** 2) Hash map */
 // time O(n)
 // space O(n)
-function majorityElement1(nums) {
+function majorityElement(nums) {
   const map = {};
-
-  for (let i = 0; i < nums.length; i++) {
-    map[nums[i]] = (map[nums[i]] || 0) + 1;
-    if (map[nums[i]] > nums.length / 2) return nums[i];
+  for (let n of nums) {
+    if (map[n] == null) map[n] = 0;
+    map[n]++;
+    if (map[n] > nums.length / 2) return n;
   }
 }
