9044f21d47ccf6bbf8802566421dd427cc731c07
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-10-06 22:02:08 -0700

    feat: Missing Ranges

---
+++ JavaScript/163. Missing Ranges.js
@@ -0,0 +1,29 @@
+// Given a sorted integer array nums, where the range of elements are in the inclusive range [lower, upper], return its missing ranges.
+//
+// Example:
+//
+// Input: nums = [0, 1, 3, 50, 75], lower = 0 and upper = 99,
+// Output: ["2", "4->49", "51->74", "76->99"]
+
+/**
+ * @param {number[]} nums
+ * @param {number} lower
+ * @param {number} upper
+ * @return {string[]}
+ */
+function findMissingRanges(nums, lower, upper) {
+  const res = [];
+  nums = [lower - 1, ...nums, upper + 1];
+
+  for (let i = 1; i < nums.length; i++) {
+    const diff = nums[i] - nums[i - 1];
+
+    if (diff === 2) {
+      res.push(`${nums[i - 1] + 1}`);
+    } else if (diff > 2) {
+      res.push(`${nums[i - 1] + 1}->${nums[i] - 1}`);
+    }
+  }
+
+  return res;
+}
