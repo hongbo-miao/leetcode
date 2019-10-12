fe107a0eb3895e50364f0da51cae67a636f4b91f
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-10-12 14:06:19 -0700

    feat: Find Minimum in Rotated Sorted Array

---
+++ JavaScript/153. Find Minimum in Rotated Sorted Array.js
@@ -0,0 +1,32 @@
+// Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+//
+// (i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+//
+// Find the minimum element.
+//
+// You may assume no duplicate exists in the array.
+//
+// Example 1:
+//
+// Input: [3,4,5,1,2]
+// Output: 1
+//
+// Example 2:
+//
+// Input: [4,5,6,7,0,1,2]
+// Output: 0
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function findMin(nums) {
+  let l = 0;
+  let r = nums.length - 1;
+  while (l < r) {
+    const m = Math.floor((l + r) / 2);
+    if (nums[m] > nums[r]) l = m + 1;
+    else r = m;
+  }
+  return nums[l];
+}
