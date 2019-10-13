9c76185e8177b832408561505df421b6668a10b4
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-10-12 18:09:50 -0700

    refact: Implement Trie (Prefix Tree)

--- JavaScript/208. Implement Trie (Prefix Tree).js
+++ JavaScript/208. Implement Trie (Prefix Tree).js
@@ -3,6 +3,11 @@
 // Note:
 // You may assume that all inputs are consist of lowercase letters a-z.
 
+// Space O(M*N*K)
+// If we have M words to insert in total and the length of words is at most N, there will be at most M*N nodes in the
+//   worst case (any two words don't have a common prefix).
+// Let's assume that there are maximum K different characters (K is equal to 26 in this problem, but might differs
+//   in different cases). So each node will maintain a map whose size is at most K.
 class Trie {
   constructor() {
     this.root = {};
@@ -13,6 +18,7 @@ class Trie {
    * @param {string} word
    * @return {void}
    */
+  // Time O(N), N is the longest length of the word
   insert(word) {
     let node = this.root;
 
@@ -42,6 +48,7 @@ class Trie {
    * @param {string} word
    * @return {boolean}
    */
+  // Time O(N)
   search(word) {
     const node = this.traverse(word);
     return node != null && node.isWord === true;
@@ -52,7 +59,9 @@ class Trie {
    * @param {string} prefix
    * @return {boolean}
    */
+  // Time O(N)
   startsWith(prefix) {
-    return this.traverse(prefix) != null;
+    const node = this.traverse(prefix);
+    return node != null;
   }
 }
