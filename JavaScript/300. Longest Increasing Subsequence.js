4f441ca5cf54521a8b65ebba6f6cc7106ff09b20
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-10-14 21:41:32 -0700

    refact: review

--- JavaScript/300. Longest Increasing Subsequence.js
+++ JavaScript/300. Longest Increasing Subsequence.js
@@ -56,6 +56,22 @@ function lengthOfLIS1(nums) {
 // Time O(n log n)
 // Space O(n)
 //
+// tails is an array storing the smallest tail of all increasing subsequences with length i+1 in tails[i].
+// For example, say we have nums = [4,5,6,3], then all the available increasing subsequences are:
+//
+// len = 1  [4], [5], [6], [3]   => tails[0] = 3
+// len = 2  [4, 5], [5, 6]       => tails[1] = 5
+// len = 3  [4, 5, 6]            => tails[2] = 6
+//
+// We can easily prove that tails is a increasing array. Therefore it is possible to do a binary search in tails array to find the one needs update.
+//
+// Each time we only do one of the two:
+//
+// (1) if x is larger than all tails, append it, increase the size by 1
+// (2) if tails[i-1] < x <= tails[i], update tails[i]
+//
+// Doing so will maintain the tails invariant. The the final answer is just the size.
+//
 // Example
 // nums = [10, 9, 2, 5, 3, 7, 101, 18]
 //
@@ -74,18 +90,16 @@ function lengthOfLIS(nums) {
   const tails = [nums[0]];
 
   for (let n of nums) {
-    let i = 0;
-    let j = tails.length;
+    let l = 0;
+    let r = tails.length;
 
-    while (j > i) {
-      const mid = ~~((i + j) / 2);
-
-      if (tails[mid] < n) i = mid + 1;
-      else j = mid;
+    while (l < r) {
+      const m = ~~((l + r) / 2);
+      if (tails[m] < n) l = m + 1;
+      else r = m;
     }
 
-    tails[j] = n;
+    tails[r] = n;
   }
-
   return tails.length;
 }
--- JavaScript/304. Range Sum Query 2D - Immutable.js
+++ JavaScript/304. Range Sum Query 2D - Immutable.js
@@ -18,6 +18,11 @@
 //   There are many calls to sumRegion function.
 //   You may assume that row1 ≤ row2 and col1 ≤ col2.
 
+/** 1) Brute force */
+// Time O(mn) time per query. Assume that mm and nn represents the number of rows and columns respectively, each
+//   sumRegion query can go through at most m * n elements.
+//
+// Space O(1). Note that data is a reference to matrix and is not a copy of it.
 class NumMatrix {
   /**
    * @param {number[][]} matrix
@@ -43,5 +48,68 @@ class NumMatrix {
     }
 
     return sum;
+  }
+}
+
+/** 2) Caching */
+// https://leetcode.com/problems/range-sum-query-2d-immutable/discuss/75350/Clean-C%2B%2B-Solution-and-Explaination-O(mn)-space-with-O(1)-time
+//
+// sums[i+1][j+1] represents the sum of area from matrix[0][0] to matrix[i][j]
+//
+// To calculate sums, the ideas as below
+//
+// +-----+-+-------+     +--------+-----+     +-----+---------+     +-----+--------+
+// |     | |       |     |        |     |     |     |         |     |     |        |
+// |     | |       |     |        |     |     |     |         |     |     |        |
+// +-----+-+       |     +--------+     |     |     |         |     +-----+        |
+// |     | |       |  =  |              |  +  |     |         |  -  |              |
+// +-----+-+       |     |              |     +-----+         |     |              |
+// |               |     |              |     |               |     |              |
+// |               |     |              |     |               |     |              |
+// +---------------+     +--------------+     +---------------+     +--------------+
+//
+//    sums[i][j]      =    sums[i-1][j]    +     sums[i][j-1]    -   sums[i-1][j-1]   +
+//
+//                         matrix[i-1][j-1]
+// So, we use the same idea to find the specific area's sum.
+//
+// +---------------+   +--------------+   +---------------+   +--------------+   +--------------+
+// |               |   |         |    |   |   |           |   |         |    |   |   |          |
+// |   (r1,c1)     |   |         |    |   |   |           |   |         |    |   |   |          |
+// |   +------+    |   |         |    |   |   |           |   +---------+    |   +---+          |
+// |   |      |    | = |         |    | - |   |           | - |      (r1,c2) | + |   (r1,c1)    |
+// |   |      |    |   |         |    |   |   |           |   |              |   |              |
+// |   +------+    |   +---------+    |   +---+           |   |              |   |              |
+// |        (r2,c2)|   |       (r2,c2)|   |   (r2,c1)     |   |              |   |              |
+// +---------------+   +--------------+   +---------------+   +--------------+   +--------------+
+class NumMatrix {
+  constructor(matrix) {
+    if (matrix == null || matrix.length === 0 || matrix[0].length === 0) return;
+
+    const h = matrix.length;
+    const w = matrix[0].length;
+
+    const dp = [];
+    for (let i = 0; i <= h; i++) {
+      const r = [];
+      for (let j = 0; j <= w; j++) r.push(null);
+      dp.push(r);
+    }
+
+    for (let i = 0; i < h; i++) {
+      for (let j = 0; j < w; j++) {
+        dp[i + 1][j + 1] = dp[i + 1][j] + dp[i][j + 1] + matrix[i][j] - dp[i][j];
+      }
+    }
+
+    this.dp = dp;
+  }
+
+
+  sumRegion(row1, col1, row2, col2) {
+    return this.dp[row2 + 1][col2 + 1]
+      - this.dp[row1][col2 + 1]
+      - this.dp[row2 + 1][col1]
+      + this.dp[row1][col1];
   }
 }
