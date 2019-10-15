65e5883aff82284983c598246ac7d8b9bab319af
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-10-14 17:52:39 -0700

    feat: new solution for Find the Duplicate Number

--- JavaScript/287. Find the Duplicate Number.js
+++ JavaScript/287. Find the Duplicate Number.js
@@ -21,27 +21,58 @@
  * @param {number[]} nums
  * @return {number}
  */
-/** Cycle detection - Floyd's Tortoise and Hare */
+
+/** 1) Sorting */
+// Time O(n)
+// Space O(n)
+function findDuplicate1(nums) {
+  const set = new Set();
+  for (let n of nums) {
+    if (set.has(n)) return n;
+    set.add(n);
+  }
+  return null;
+}
+
+/** 2) Sorting */
+// Time O(n log n)
+// Space O(1)
+function findDuplicate2(nums) {
+  nums.sort((a, b) => a - b);
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) return nums[i];
+  }
+  return null;
+}
+
+/** 3) Cycle detection - Floyd's Tortoise and Hare */
 // Similar
 // 142. Linked List Cycle II
 //
 // Time O(n)
 // Space O(1)
 function findDuplicate(nums) {
-  let slow = nums[0];
-  let fast = nums[nums[0]];
+  function getIntersect(head) {
+    let slow = head;
+    let fast = head;
 
-  while (slow !== fast) {
-    slow = nums[slow];
-    fast = nums[nums[fast]];
+    while (fast != null && nums[fast] != null) {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+      if (slow === fast) return fast;
+    }
+    return null;
   }
 
-  slow = 0;
+  const intersect = getIntersect(nums[0]);
+  if (intersect == null) return null;
+
+  let slow = nums[0];
+  let fast = intersect;
 
   while (slow !== fast) {
     slow = nums[slow];
     fast = nums[fast];
   }
-
-  return slow;
+  return fast;
 }
