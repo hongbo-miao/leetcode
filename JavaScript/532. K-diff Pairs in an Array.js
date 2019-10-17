ab2cb54e6922a8b440a0ca09022f2b662b6a5830
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-10-16 18:45:32 -0700

    feat: K-diff Pairs in an Array

---
+++ JavaScript/532. K-diff Pairs in an Array.js
@@ -0,0 +1,85 @@
+// Given an array of integers and an integer k, you need to find the number of unique k-diff pairs in the array. Here a k-diff pair is defined as an integer pair (i, j), where i and j are both numbers in the array and their absolute difference is k.
+//
+// Example 1:
+// Input: [3, 1, 4, 1, 5], k = 2
+// Output: 2
+// Explanation: There are two 2-diff pairs in the array, (1, 3) and (3, 5).
+// Although we have two 1s in the input, we should only return the number of unique pairs.
+//
+// Example 2:
+// Input:[1, 2, 3, 4, 5], k = 1
+// Output: 4
+// Explanation: There are four 1-diff pairs in the array, (1, 2), (2, 3), (3, 4) and (4, 5).
+//
+// Example 3:
+// Input: [1, 3, 1, 5, 4], k = 0
+// Output: 1
+// Explanation: There is one 0-diff pair in the array, (1, 1).
+//
+// Note:
+// The pairs (i, j) and (j, i) count as the same pair.
+// The length of the array won't exceed 10,000.
+// All the integers in the given input belong to the range: [-1e7, 1e7].
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+
+/** 1) Binary search */
+// Time O(n log n)
+// Space O(1)
+function findPairs1(nums, k) {
+  function binarySearch(l, r, target) {
+    while (l <= r) {
+      const m = ~~((l + r) / 2);
+      if (nums[m] === target) return true;
+      else if (nums[m] < target) l = m + 1;
+      else r = m - 1;
+    }
+    return false;
+  }
+
+  nums.sort((a, b) => a - b);
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) continue;  // avoid duplicates to get unique pairs
+    if (binarySearch(i + 1, nums.length - 1, nums[i] + k)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+/** 2) Hash map */
+// Time O(n)
+// Space O(n)
+function findPairs(nums, k) {
+  if (k < 0) return 0;
+
+  const map = new Map();
+
+  for (let num of nums) {
+    if (map.has(num)) {
+      map.set(num, map.get(num) + 1);
+    } else {
+      map.set(num, 1);
+    }
+  }
+
+  let res = 0;
+  for (let [num] of map.entries()) {
+    if (k === 0) {
+      if (map.get(num) >= 2) {
+        res++;
+      }
+    } else {
+      if (map.has(num + k)) {
+        res++;
+      }
+    }
+  }
+
+  return res;
+}
