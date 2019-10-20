cf6dcc28e0821d9ea41dcd770296c14f9b6ed24f
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-10-20 14:04:46 -0700

    feat: Rearrange String k Distance Apart

---
+++ JavaScript/0358. Rearrange String k Distance Apart.js
@@ -0,0 +1,30 @@
+// Given a non-empty string s and an integer k, rearrange the string such that the same characters are at least distance k from each other.
+//
+// All input strings are given in lowercase letters. If it is not possible to rearrange the string, return an empty string "".
+//
+// Example 1:
+//
+// Input: s = "aabbcc", k = 3
+// Output: "abcabc"
+// Explanation: The same letters are at least distance 3 from each other.
+//
+// Example 2:
+//
+// Input: s = "aaabc", k = 3
+// Output: ""
+// Explanation: It is not possible to rearrange the string.
+//
+// Example 3:
+//
+// Input: s = "aaadbbcc", k = 2
+// Output: "abacabcd"
+// Explanation: The same letters are at least distance 2 from each other.
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+
+/** Priority queue */
+// JavaScript is lack of priority queue, check Python version
