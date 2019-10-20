9c61da6fef761b708634fd4765e5db6ec97a97af
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-10-19 19:23:18 -0700

    feat: Minimum Cost to Connect Sticks

---
+++ JavaScript/1167. Minimum Cost to Connect Sticks.js
@@ -0,0 +1,26 @@
+// You have some sticks with positive integer lengths.
+// You can connect any two sticks of lengths X and Y into one stick by paying a cost of X + Y. You perform this action until there is one stick remaining.
+// Return the minimum cost of connecting all the given sticks into one stick in this way.
+//
+// Example 1:
+//
+// Input: sticks = [2,4,3]
+// Output: 14
+//
+// Example 2:
+//
+// Input: sticks = [1,8,3,5]
+// Output: 30
+//
+// Constraints:
+//
+// 1 <= sticks.length <= 10^4
+// 1 <= sticks[i] <= 10^4
+
+/**
+ * @param {number[]} sticks
+ * @return {number}
+ */
+
+/** Priority queue */
+// JavaScript is lack of priority queue, check Python version
