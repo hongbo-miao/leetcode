3e1181879f93c4bec4b4c5ea78d368449fda4a87
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-10-25 11:38:19 -0700

    feat: Defanging an IP Address

---
+++ JavaScript/1108. Defanging an IP Address.js
@@ -0,0 +1,24 @@
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+// A defanged IP address replaces every period "." with "[.]".
+//
+// Example 1:
+//
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+//
+// Example 2:
+//
+// Input: address = "255.100.50.0"
+// Output: "255[.]100[.]50[.]0"
+//
+// Constraints:
+//
+// The given address is a valid IPv4 address.
+
+/**
+ * @param {string} address
+ * @return {string}
+ */
+function defangIPaddr(address) {
+  return address.replace(/\./g, '[.]');
+}
