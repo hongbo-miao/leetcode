e3fa016708814ed54d995bc8fbad9e17acf0f8c9
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-10-25 00:29:09 -0700

    feat: Remove Invalid Parentheses

---
+++ JavaScript/0301. Remove Invalid Parentheses.js
@@ -0,0 +1,47 @@
+// Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
+//
+// Note: The input string may contain letters other than the parentheses ( and ).
+//
+// Example 1:
+//
+// Input: "()())()"
+// Output: ["()()()", "(())()"]
+//
+// Example 2:
+//
+// Input: "(a)())()"
+// Output: ["(a)()()", "(a())()"]
+//
+// Example 3:
+//
+// Input: ")("
+// Output: [""]
+
+/** BFS */
+function removeInvalidParentheses(s) {
+  let q = new Set([s]);
+  while (q.size) {
+    const q2 = new Set();
+    for (let v of q) {
+      if (isValid(v)) {
+        return Array.from(q).filter(isValid);
+      }
+
+      for (let i = 0; i < v.length; i++) {
+        q2.add(v.slice(0, i) + v.slice(i+1));
+      }
+    }
+    q = q2;
+  }
+  return [''];
+}
+
+function isValid(s) {
+  let bal = 0;
+  for (let c of s) {
+    if (c === '(') bal++;
+    else if (c === ')') bal--;
+    if (bal < 0) return false;
+  }
+  return bal === 0;
+}
