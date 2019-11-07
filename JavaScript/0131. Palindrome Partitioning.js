8ab8683927620e0a72f8d9201f7b3f006cb3977d
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-11-07 15:58:58 -0800

    feat: Palindrome Partitioning

---
+++ JavaScript/0131. Palindrome Partitioning.js
@@ -0,0 +1,45 @@
+// Given a string s, partition s such that every substring of the partition is a palindrome.
+// Return all lsible palindrome partitioning of s.
+//
+// Example:
+//
+// Input: "aab"
+// Output:
+// [
+//   ["aa","b"],
+//   ["a","a","b"]
+// ]
+
+/**
+ * @param {string} s
+ * @return {string[][]}
+ */
+
+/** Backtracking */
+const partition = (s) => {
+  const res = [];
+
+  const isPalindrome = (l, r) => {
+    while (l < r) {
+      if (s[l] !== s[r]) return false;
+      l++;
+      r--;
+    }
+    return true;
+  };
+
+  const go = (l, list) => {
+    if (l === s.length) {
+      res.push(list);
+    } else {
+      for (let r = l; r < s.length; r++) {
+        if (isPalindrome(l, r)) {
+          go(r + 1, [...list, s.slice(l, r + 1)]);
+        }
+      }
+    }
+  };
+
+  go(0, []);
+  return res;
+};
