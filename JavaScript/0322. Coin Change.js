1eea26b3b1d3ac87c72cda168432ab7330924a5b
Hongbo Miao <Hongbo.Miao@outlook.com>
2019-11-11 22:40:57 -0800

    refact: reivew

--- JavaScript/0322. Coin Change.js
+++ JavaScript/0322. Coin Change.js
@@ -65,11 +65,11 @@ const coinChange1 = (coins, amount) => {
   const dp = Array(amount + 1).fill(Infinity);
   dp[0] = 0;
   for (let i = 1; i <= amount; i++) {
-    for (let j = 0; j < coins.length; j++) {
-      if (i - coins[j] >= 0) {
+    for (const coin of coins) {
+      if (i - coin >= 0) {
         dp[i] = Math.min(
           dp[i],
-          dp[i - coins[j]] + 1,
+          dp[i - coin] + 1,
         );
       }
     }
