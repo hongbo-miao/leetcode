391ced12a418eb0f34db414634ace690e3d812cc
Hongbo Miao <Hongbo.Miao@outlook.com>
2022-03-15 00:53:11 -0700

    feat(python): 0001. Two Sum

--- Python/0001. Two Sum.py
+++ Python/0001. Two Sum.py
@@ -1,27 +1,51 @@
-# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-#
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
 #
-# Example:
+# Example 1:
 #
-# Given nums = [2, 7, 11, 15], target = 9,
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 #
-# Because nums[0] + nums[1] = 2 + 7 = 9,
-# return [0, 1].
+# Example 2:
+#
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+#
+# Example 3:
+#
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+#
+# Constraints:
+#
+# 2 <= nums.length <= 10^4
+# -10^9 <= nums[i] <= 10^9
+# -10^9 <= target <= 10^9
+# Only one valid answer exists.
+#
+# Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 
 
+# 1) Brute force
+# Time O(n^2)
+# Space O(1)
 class Solution:
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+# 2)
+# Time O(n)
+# Space O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         cache = {}
         for i in range(len(nums)):
-            if nums[i] not in map:
-                cache[target - nums[i]] = i
-            else:
+            if nums[i] in cache:
                 return cache[nums[i]], i
-
-        return -1, -1
+            cache[target - nums[i]] = i
